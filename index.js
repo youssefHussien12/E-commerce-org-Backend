8e510e5091893e283438e3fef57f23254a21c498
youssef Hussien <yossefhussien@gmail.com>
2026-02-08 20:35:10 +0200

    new stripe add

--- index.js
+++ index.js
@@ -9,17 +9,23 @@ import { globalError } from "./src/middleware/globalError.js";
 import { AppError } from "./src/utils/appError.js";
 import cors from 'cors'
 import 'dotenv/config'
+import { catchError } from "./src/middleware/catchError.js";
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const app = express()
 const port = process.env.PORT || 3000
 dbConn()
 app.use(cors())
 
-
-
-
-
-
-
+app.post("/api/webhook" , express.raw( { type: 'application/json' }),catchError(async (req, res, next) => {
+    const sig = req.headers['stripe-signature'];
+    const event = stripe.webhooks.constructEvent(req.body,sig, process.env.STRIPE_WEBHOOK_SECRET);
+    let checkout 
+    if(event.type == 'checkout.session.completed'){
+        checkout = event.data.object
+    }
+    res.status(200).json({ message: "success", checkout })
+}))
 
 
 
